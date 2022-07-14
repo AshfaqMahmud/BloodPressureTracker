66f594c9b1fd03609aecfa41f5eda186cd07c712
AshfaqMahmud <ash.mse21@gmail.com>
2022-07-13 19:22:48 -0700

    sqliteconnection and insertion

--- app/src/main/java/com/example/bloodpressuretracker/MainActivity.java
+++ app/src/main/java/com/example/bloodpressuretracker/MainActivity.java
@@ -2,16 +2,28 @@ package com.example.bloodpressuretracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     SQLiteDB sqLiteDB;
+    Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add=findViewById(R.id.addbutton);
         sqLiteDB=new SQLiteDB(this);
         SQLiteDatabase sqLiteDatabase= sqLiteDB.getWritableDatabase();
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this,InsertToDB.class);
+                startActivity(intent);
+            }
+        });
     }
 }
