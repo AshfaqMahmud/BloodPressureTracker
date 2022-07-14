48489e2e8fbcb5e987cbfee6aea81b64f407270c
AshfaqMahmud <ash.mse21@gmail.com>
2022-07-13 17:56:46 -0700

    sqliteconnection

--- app/src/main/java/com/example/bloodpressuretracker/MainActivity.java
+++ app/src/main/java/com/example/bloodpressuretracker/MainActivity.java
@@ -6,10 +6,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    SQLiteDB sqLiteDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sqLiteDB=new SQLiteDB(this);
+        SQLiteDatabase sqLiteDatabase= sqLiteDB.getWritableDatabase();
     }
 }
