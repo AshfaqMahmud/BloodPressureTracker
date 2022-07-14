0150cdff3278cdabf8db48e8d49a62fc34984d70
AshfaqMahmud <ash.mse21@gmail.com>
2022-07-13 17:46:04 -0700

    initial commit

---
+++ app/src/main/java/com/example/bloodpressuretracker/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.bloodpressuretracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
