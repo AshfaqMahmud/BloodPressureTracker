4142414e46cdee2d8763176d166c38007ce73882
Ashfaq Mahmud <ash.mse21@gmail.com>
2023-02-04 03:50:16 +0600

    final-commit

--- app/src/main/java/com/example/bloodpressuretracker/MainActivity.java
+++ app/src/main/java/com/example/bloodpressuretracker/MainActivity.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -22,29 +23,31 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+
 //uml
 public class MainActivity extends AppCompatActivity {
 
     Button insert, show;
-    EditText eSys,eDia,eBp,eCmnt;
+    EditText eSys, eDia, eBp, eCmnt;
     DatabaseReference databaseReference;
+    FirebaseAuth mAuth;
     @Override
     //uml
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        insert=findViewById(R.id.add);
-        show=findViewById(R.id.view);
-        eSys=findViewById(R.id.systole);
-        eDia=findViewById(R.id.diastole);
-        eBp=findViewById(R.id.bpm);
-        eCmnt=findViewById(R.id.comment);
+        insert = findViewById(R.id.add);
+        show = findViewById(R.id.view);
+        eSys = findViewById(R.id.systole);
+        eDia = findViewById(R.id.diastole);
+        eBp = findViewById(R.id.bpm);
+        eCmnt = findViewById(R.id.comment);
 
-        Calendar calendar=Calendar.getInstance();
-        String currentDate= DateFormat.getInstance().format(calendar.getTime());
-        TextView textViewDate=findViewById(R.id.showDate);
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getInstance().format(calendar.getTime());
+        TextView textViewDate = findViewById(R.id.showDate);
         textViewDate.setText(currentDate);
-        databaseReference= FirebaseDatabase.getInstance().getReference();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
 
         insert.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,60 +58,48 @@ public class MainActivity extends AppCompatActivity {
         show.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,UserList.class));
+                startActivity(new Intent(MainActivity.this, UserList.class));
                 finish();
             }
         });
     }
-//uml
+
+    //uml
     private void InsertData() {
-        String usersys= eSys.getText().toString();
-        String userdia= eDia.getText().toString();
-        String userbp= eBp.getText().toString();
-        String usercmn= eCmnt.getText().toString();
-        Calendar calendar=Calendar.getInstance();
-        String currentDate= DateFormat.getInstance().format(calendar.getTime());
+        String usersys = eSys.getText().toString();
+        String userdia = eDia.getText().toString();
+        String userbp = eBp.getText().toString();
+        String usercmn = eCmnt.getText().toString();
+        Calendar calendar = Calendar.getInstance();
+        String currentDate = DateFormat.getInstance().format(calendar.getTime());
         int n1 = Integer.parseInt(usersys);
         int n2 = Integer.parseInt(userdia);
         int n3 = Integer.parseInt(userbp);
-        String verdict="";
-        if(usersys.isEmpty()||userdia.isEmpty()||userbp.isEmpty())
-        {
-            Toast.makeText(MainActivity.this,"Please Insert Values",Toast.LENGTH_SHORT).show();
-        }
-        else if(n1<90 || n1>140 )
-        {
-            Toast.makeText(MainActivity.this,"Systole should be in between 90 and 140",Toast.LENGTH_SHORT).show();
-        }
-        else if(n2<60 || n2>90)
-        {
-            Toast.makeText(MainActivity.this,"Diastole should be in between 60 and 90",Toast.LENGTH_SHORT).show();
-        }
-        else if(n3<50)
-        {
-            verdict="Too Low BPM";
-        }
+        String verdict = "";
+        if (usersys.isEmpty() || userdia.isEmpty() || userbp.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Please Insert Values", Toast.LENGTH_SHORT).show();
+        } else if (n1 < 90 || n1 > 140) {
+            Toast.makeText(MainActivity.this, "Systole should be in between 90 and 140", Toast.LENGTH_SHORT).show();
+        } else if (n2 < 60 || n2 > 90) {
+            Toast.makeText(MainActivity.this, "Diastole should be in between 60 and 90", Toast.LENGTH_SHORT).show();
+        } else if (n3 < 50) {
+            verdict = "Too Low BPM";
+        } else {
+            String id = databaseReference.push().getKey();
 
-        else
-        {
-            String id= databaseReference.push().getKey();
-
-            if(n1<=120 && n2<=80)
-            {
-                verdict = verdict +" Normal";
+            if (n1 <= 120 && n2 <= 80) {
+                verdict = verdict + " Normal";
+            } else {
+                verdict = verdict + " High Pressure";
             }
-            else
-            {
-                verdict = verdict+ " High Pressure";
-            }
-
-            User user= new User(usersys,userdia,userbp,usercmn,currentDate,verdict,id);
-            databaseReference.child("users").child(id).setValue(user)
+            mAuth = FirebaseAuth.getInstance();
+            String uid = mAuth.getUid();
+            User user = new User(usersys, userdia, userbp, usercmn, currentDate, verdict, id);
+            databaseReference.child("users").child(uid).child("Records").child(id).setValue(user)
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
-                            if(task.isSuccessful())
-                            {
+                            if (task.isSuccessful()) {
                                 Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
                             }
                         }
--- app/src/main/java/com/example/bloodpressuretracker/UserList.java
+++ app/src/main/java/com/example/bloodpressuretracker/UserList.java
@@ -8,8 +8,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,13 +28,14 @@ public class UserList extends AppCompatActivity implements RecyclerViewInterface
     RecyclerView recyclerView;
     ArrayList<User> list;
     DatabaseReference databaseReference;
+    FirebaseUser mAuth;
     MyAdapter myAdapter;
 
     @Override
     //ovverinding the back button
     public void onBackPressed() {
         super.onBackPressed();
-        startActivity(new Intent(UserList.this,MainActivity.class));
+        startActivity(new Intent(UserList.this, MainActivity.class));
         finish();
     }
 
@@ -38,32 +44,37 @@ public class UserList extends AppCompatActivity implements RecyclerViewInterface
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
 
-        recyclerView=findViewById(R.id.recycler);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab_user = findViewById(R.id.fab_user);
+        recyclerView = findViewById(R.id.recycler);
+        mAuth = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = mAuth.getUid();
         //getting the database reference
-        databaseReference= FirebaseDatabase.getInstance().getReference("users");
-        list=new ArrayList<>();
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        list = new ArrayList<>();
 
         //bringing list in the recycler view
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        myAdapter=new MyAdapter(this,list,this);
+        myAdapter = new MyAdapter(this, list, this);
         recyclerView.setAdapter(myAdapter);
 
-        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+
+        databaseReference.child(uid).child("Records").addListenerForSingleValueEvent(new ValueEventListener() {
             @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                for (DataSnapshot dataSnapshot:snapshot.getChildren())
-                {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     String key = dataSnapshot.getKey();
 
-                    String systol=dataSnapshot.child("sys").getValue(String.class);
-                    String diastol=dataSnapshot.child("dia").getValue(String.class);
-                    String bpm=dataSnapshot.child("bp").getValue(String.class);
-                    String date=dataSnapshot.child("date").getValue(String.class);
-                    String cmnt=dataSnapshot.child("cmnt").getValue(String.class);
-                    String verdict=dataSnapshot.child("verdict").getValue(String.class);
+                    String systol = dataSnapshot.child("sys").getValue(String.class);
+                    String diastol = dataSnapshot.child("dia").getValue(String.class);
+                    String bpm = dataSnapshot.child("bp").getValue(String.class);
+                    String date = dataSnapshot.child("date").getValue(String.class);
+                    String cmnt = dataSnapshot.child("cmnt").getValue(String.class);
+                    String verdict = dataSnapshot.child("verdict").getValue(String.class);
                     //Toast.makeText(UserList.this,"ADEED",Toast.LENGTH_SHORT).show();
-                    User user= new User(systol,diastol,bpm,cmnt,date,verdict,key);
+                    User user = new User(systol, diastol, bpm, cmnt, date, verdict, key);
                     list.add(user);                                     //inserting value in the firebase
                 }
                 myAdapter.notifyDataSetChanged();                       //adapter noitified for data change
@@ -71,7 +82,23 @@ public class UserList extends AppCompatActivity implements RecyclerViewInterface
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(UserList.this," "+error.toString(),Toast.LENGTH_SHORT).show();
+                Toast.makeText(UserList.this, " " + error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserList.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        fab_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(UserList.this, ProfilePage.class);
+                startActivity(intent);
             }
         });
 
@@ -81,13 +108,13 @@ public class UserList extends AppCompatActivity implements RecyclerViewInterface
     //clickable items on the recyclerview
     @Override
     public void onItemClick(int position) {
-        Intent intent=new Intent(UserList.this,ShowDetails.class);
-        intent.putExtra("Key",list.get(position).getKey());
-        intent.putExtra("Systole",list.get(position).getSys());
-        intent.putExtra("Diastole",list.get(position).getDia());
-        intent.putExtra("BPM",list.get(position).getBp());
-        intent.putExtra("Date",list.get(position).getDate());
-        intent.putExtra("Comment",list.get(position).getCmnt());
+        Intent intent = new Intent(UserList.this, ShowDetails.class);
+        intent.putExtra("Key", list.get(position).getKey());
+        intent.putExtra("Systole", list.get(position).getSys());
+        intent.putExtra("Diastole", list.get(position).getDia());
+        intent.putExtra("BPM", list.get(position).getBp());
+        intent.putExtra("Date", list.get(position).getDate());
+        intent.putExtra("Comment", list.get(position).getCmnt());
         startActivity(intent);
         finish();
 
